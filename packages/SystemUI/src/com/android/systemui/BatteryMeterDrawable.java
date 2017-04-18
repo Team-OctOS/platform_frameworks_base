GZR-ValidusOS/frameworks_base

Battery icon: fix getDrawable for new Android API
Teamwork as usual with awesome substratum guys

this fixes:

12-23 01:08:01.060  6026  6026 W Resources: Drawable com.android.systemui:drawable/ic_battery_portrait has unresolved theme attributes! Consider using Resources.getDrawable(int, Theme) or Context.getDrawable(int).
12-23 01:08:01.060  6026  6026 W Resources: java.lang.RuntimeException
12-23 01:08:01.060  6026  6026 W Resources: 	at android.content.res.Resources.getDrawable(Resources.java:746)
12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterDrawable.loadBatteryDrawables(BatteryMeterDrawable.java:450)
12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterDrawable.<init>(BatteryMeterDrawable.java:184)
12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterDrawable.<init>(BatteryMeterDrawable.java:146)
12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterView.<init>(BatteryMeterView.java:59)
12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterView.<init>(BatteryMeterView.java:49)

Change-Id: I251b2a9ae71c4b337843c347c07dc24c5b8a64c6

packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -513,7 +513,7 @@ public class BatteryMeterDrawable extends Drawable implements
 
     private void loadBatteryDrawables(Resources res, int style) {
         final int drawableResId = getBatteryDrawableResourceForStyle(style);
-        mBatteryDrawable = (LayerDrawable) res.getDrawable(drawableResId);
+        mBatteryDrawable = (LayerDrawable) res.getDrawable(drawableResId, null);
         mFrameDrawable = mBatteryDrawable.findDrawableByLayerId(R.id.battery_frame);
         mFrameDrawable.setTint(mCurrentBackgroundColor != 0
                 ? mCurrentBackgroundColor : res.getColor(R.color.batterymeter_frame_color));
@@ -528,7 +528,7 @@ public class BatteryMeterDrawable extends Drawable implements
         final int resId = getBatteryDrawableResourceForStyle(style);
         final Drawable batteryDrawable;
         try {
-            batteryDrawable = res.getDrawable(resId);
+            batteryDrawable = res.getDrawable(resId, null);
         } catch (Resources.NotFoundException e) {
             throw new BatteryMeterDrawableException(res.getResourceName(resId) + " is an " +
                     "invalid drawable", e);

Team-OctOS/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -424,7 +424,7 @@ public class BatteryMeterDrawable extends Drawable implements
         }
 
         final int drawableResId = getBatteryDrawableResourceForStyle(style);
-        mBatteryDrawable = (LayerDrawable) mContext.getDrawable(drawableResId);
+        mBatteryDrawable = (LayerDrawable) mContext.getDrawable(drawableResId, null);
         mFrameDrawable = mBatteryDrawable.findDrawableByLayerId(R.id.battery_frame);
         mFrameDrawable.setTint(mCurrentBackgroundColor != 0
                 ? mCurrentBackgroundColor : res.getColor(R.color.batterymeter_frame_color));
@@ -444,7 +444,7 @@ public class BatteryMeterDrawable extends Drawable implements
         final int resId = getBatteryDrawableResourceForStyle(style);
         final Drawable batteryDrawable;
         try {
-            batteryDrawable = mContext.getDrawable(resId);
+            batteryDrawable = mContext.getDrawable(resId, null);
         } catch (Resources.NotFoundException e) {
             throw new BatteryMeterDrawableException(res.getResourceName(resId) + " is an " +
                     "invalid drawable", e);